d4058cee0749cc055b609483bca523998902dbd0
Adam Richards <adam@adamr.co.uk>
2022-12-02 09:32:11 +0000

    day 2 complete

--- 02.py
+++ 02.py
@@ -13,25 +13,55 @@ drawtable = {
     'Z': 'C'   
 }
 
+losetable = {
+    'X': 'B',
+    'Y': 'C',
+    'Z': 'A'   
+}
+
 guessvalues = {
     'X': 1,
     'Y': 2,
-    'Z': 3
+    'Z': 3,
+    'A': 1,
+    'B': 2,
+    'C': 3
 }
+
 score = 0
 WIN = 6
 DRAW = 3
 
-for row in data:
-    myguess = row[2]
+
+# PART 1
+for row in data:                               
+    myguess = row[2]                            # split this row into my guess and theirs
     theirguess = row[0]
-    if theirguess == drawtable[myguess]:
+    if theirguess == drawtable[myguess]:        # is it a draw?
         score += (DRAW + guessvalues[myguess])
-        print('draw')
-    elif wintable[myguess] == theirguess:
+    elif wintable[myguess] == theirguess:       # a win?
         score += (WIN + guessvalues[myguess])
-        print('win')
-    else:
+    else:                                       # guess not, must be a loss
         score += guessvalues[myguess]
-        print('lose')
 print(score)
+
+
+# PART 2
+score2 = 0
+
+for row in data:
+    theirguess = row[0]
+    instruction = row[2]
+    if instruction == 'X':  
+        for k, v in losetable.items():              # use the loss table to find what would have beaten my guess
+            if v == theirguess:
+                score2 += guessvalues[k]
+    elif instruction == 'Y': 
+        score2 += guessvalues[theirguess] + DRAW
+    else:
+        for k, v in wintable.items():
+            if v == theirguess:
+                score2 += guessvalues[k] + WIN      # use the win table to find out what would have beaten their guess
+
+
+print(score2)
