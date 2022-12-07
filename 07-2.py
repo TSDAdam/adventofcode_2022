ca2d0913dc3866da656bc32ec20e25009e0a567c
Adam Richards <adam@adamr.co.uk>
2022-12-07 15:10:55 +0000

    day 7 commented

--- 07-2.py
+++ 07-2.py
@@ -2,29 +2,30 @@
 with open ('./07.in') as file:
     data = [line.strip() for line in file]
 
-content = dict({"root": 0})
+filesizes = dict({"root": 0})
 
 for row in data:
     if row == "$ cd /":
-        currentDir = ["root"]
+        currentDir = ["root"]   # start at the first entry, add it to a list of the paths, and call that first one 'root'
     elif row[0:7] == "$ cd ..":
-        currentDir.pop()
+        currentDir.pop()        # if it's cd .. , take the last path off the stack off paths
     elif row[0:4] == "$ cd":
-        currentDir.append("/".join([currentDir[-1], row[5:]]))
-        content[currentDir[-1]] = 0
-    elif row[0].isdigit():
-        for file in currentDir:
-            content[file] += int(row.split(" ")[0])
+        currentDir.append("/".join([currentDir[-1], row[5:]]))  # if we move up to a new dir, add it to the stack, joined with a '/'
+        filesizes[currentDir[-1]] = 0                           # ...and start its filesize total at 0
+    elif row[0].isdigit():                                      # if the first char is a digit, it must be a file size
+        for file in currentDir:                                 # the tricky bit! if it's a file, 
+            filesizes[file] += int(row.split(" ")[0])           # add it's value to every path in the tree as it stands.
             #print (content)
 p1 = 0
-for x in content.values():
-    if x <= 100000:
-        p1 += x
-p2 = []
-for y in content.values():
-    if y >= content['root'] - 40000000:
-        p2.append(y)
+for x in filesizes.values():                # part 1 - iterate through all the values
+    if x <= 100000:                         # if that path is at most 100k
+        p1 += x                             # add it to the total
 
+p2 = []
+for y in filesizes.values():                # part 2 - get all the values again
+    if y >= filesizes['root'] - 40000000:   # we know the disk is 70M - so take the full occupied space at root,
+        p2.append(y)                        # subtract 40M, and if your path is bigger, then it must free up the space
+                                            # and make a list of those values  
 print(p1)
 
-print(min(p2))
+print(min(p2))                              # part two wants the smallest matching dir to delete, so use min()
