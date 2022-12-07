6067a4d822a32dcf65357359c9e443ff00fd4007
Adam Richards <adam@adamr.co.uk>
2022-12-07 12:02:11 +0000

    day 7 stuck on part 1 - recursion

--- 07.py
+++ 07.py
@@ -1,17 +1,63 @@
-with open ('./07x.in') as file:
+from collections import defaultdict
+
+with open ('./07.in') as file:
     data = [line.strip() for line in file]
 
-dirs = {"/": ""}
+contains = defaultdict(list)
+containedIn = defaultdict(list)
+dirsizes = defaultdict(int)
 
-def ls(contents):
-    return
-
+def ls(pwd, contents):
+    for row in contents:
+        entry, arg = row.split(" ")
+        if entry == "dir":              # if this row is a dir
+            contains[pwd].append(arg)   # add it to the list of dirs the current dir contains
+            containedIn[arg].append(pwd)
+        else:
+            dirsizes[pwd] += int(entry)
+            # print(contains, dirsizes)
 
 i = 0
-text, values = data[i].split(" ")
-if text == "$":
-    command, arg = values.split(" ")
-    if command == "cd":
-        pwd = arg 
+while i < len(data) -1:
+    firstspace = data[i].find(" ")
+    text, values = data[i][:firstspace], data[i][firstspace+1:]
+    if text == "$":         # ENTRY BY THE USER
+        command = values[:2]
+        if command == "cd":     
+            directory = values[3:]
+            if directory != "..":
+                pwd = directory    # Change to the called dir if not ..
+            else: 
+                pwd = containedIn[directory]  # Else check the contained in dir to see where it lives, and jump back up
+            i += 1
+        elif command == "ls":
+            i += 1
+            contents = []
+            while data[i][0] != "$" and i < len(data) -1:
+                contents.append(data[i])
+                i += 1
+            ls(pwd, contents)
+            
+print(dirsizes)
+print(contains)
 
-elif text == 
+deepdirsize = defaultdict(int)
+
+def getsubdirsize(currentdir):
+    deepdirsize[currentdir] = dirsizes[currentdir]
+    if currentdir in contains:
+        for subdir in contains[currentdir]:
+            deepdirsize[currentdir] += getsubdirsize(subdir)
+    return dirsizes[currentdir]
+
+t=0
+for subdir in contains['/']:
+    getsubdirsize(subdir)
+
+print(deepdirsize)
+
+p1 = 0
+for dir, size in deepdirsize.items():
+    if size <= 100000:
+        p1 += size
+print(p1)
